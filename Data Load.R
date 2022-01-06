e6f0fc660d125b4aa9577604ce2ae92ea5ab07ce
benKeener <36938699+benKeener@users.noreply.github.com>
2022-01-06 01:09:22 -0500

    Update Data Load.R

--- Data Load.R
+++ Data Load.R
@@ -8,17 +8,3 @@ full_pbp <- purrr::map_df(seasons, function(x) {
 })
 
 
-pbp <- pbp %>%
-  mutate(passer_player_name = ifelse(rusher_player_name == "G.Minshew", "G.Minshew II", rusher_player_name))
-
-f20 <- f20 %>%
-  mutate(passer_player_name = ifelse(passer_player_name == "M.Ingram", "M.Ingram II", passer_player_name))
-
-f20 <- readRDS(
-  url(
-    glue::glue("https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/play_by_play_2020.rds")
-  )
-)
-
-
-
